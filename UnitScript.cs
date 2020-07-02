720c1bc03215bdf2281ffa9fbffd277271293e19
co1-orful <65449272+co1-orful@users.noreply.github.com>
2020-07-02 18:25:08 +0300

    Movement System 1 Update
    - More keys for controls (arrows/Space and WASD)
    - No spamming Space for flying

--- UnitScript.cs
+++ UnitScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,16 +6,26 @@ public class UnitScript : MonoBehaviour {
 
 	public GameObject obj;
 	public float range = 5f, moveSpeed = 3f, jumpHeight = 10f;
+	private bool inAir;
 
 	void Update () {
-		if(Input.GetKey(KeyCode.Space))
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(0,1), ForceMode2D.Impulse);
-		  
-		if(Input.GetKey(KeyCode.LeftArrow))
+		if((Input.GetKey(KeyCode.Space) | Input.GetKey(KeyCode.W)) && !inAir){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+			inAir = true;
+			}
+
+		if(Input.GetKey(KeyCode.LeftArrow) | Input.GetKey(KeyCode.A))
 			obj.transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
 
-		if(Input.GetKey(KeyCode.RightArrow))
+		if(Input.GetKey(KeyCode.RightArrow) | Input.GetKey(KeyCode.D))
 			obj.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+
 		}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            inAir = false;
+    }
 
 }
